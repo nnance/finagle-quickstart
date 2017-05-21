1318c9cf7815079eadc4c6d8ac51c3bf9e4efb68
Nick Nance <nick.nance@creditkarma.com>
2017-05-21 16:30:05 -0700

    twitter proxy

--- src/main/scala/Client.scala
+++ src/main/scala/Client.scala
@@ -3,9 +3,9 @@ import com.twitter.finagle.http
 import com.twitter.util.{Await, Future}
 
 object Client extends App {
-  val client: Service[http.Request, http.Response] = Http.newService("www.scala-lang.org:80")
+  val client: Service[http.Request, http.Response] = Http.newService("localhost:8080")
   val request = http.Request(http.Method.Get, "/")
-  request.host = "www.scala-lang.org"
+  request.host = "localhost"
   val response: Future[http.Response] = client(request)
   Await.result(response.onSuccess { rep: http.Response =>
     println("GET success: " + rep)
